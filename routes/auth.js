7e58228a14910d90dcf7d18fecec339e4ebea28f
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-07-09 13:04:11 -0300

    configure local strategy to verify password and authenticate user

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,24 @@ const db = require('../db');
 
 const router = express.Router();
 
+passport.use(new LocalStrategy((username, password, cb) => {
+    db.get('SELECT * FROM users WHERE username = ?', [ username ], (err, row) => {
+        if (err) { return cb(err) };
+
+        if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }) };
+
+        crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', (err, hashedPassword) => {
+            if (err) { return cb(err) };
+        
+            if (!crypto.timingSafeEqual(row.hashedPassword, hashedPassword)) { 
+                return cb(null, false, { message: 'Incorrect username or password.' })
+            };
+        });
+
+        return cb(null, row);
+    });
+}));
+
 router.get('/login', (req, res, next) => {
     res.render('login');
 });
